281fb434a0afbe284163cbaa691489c55e61189b
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-06-08 23:19:43 +0900

    fix : correct model namespace

--- app/Filament/Resources/ArchiveTypeResource.php
+++ app/Filament/Resources/ArchiveTypeResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ArchiveTypeResource\Pages;
 use App\Filament\Resources\ArchiveTypeResource\RelationManagers;
-use App\Models\ArchiveType;
+use App\Models\Archivetype;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
