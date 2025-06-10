3583bcc38fba4a8ec723e78d544c852711bda159
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-06-10 14:50:45 +0900

    update: add Home.vue component with Laravel and PHP version display

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// Define any props passed from Laravel here
+defineProps<{
+    laravelVersion: string;
+    phpVersion: string;
+}>();
+</script>
+
+<template>
+    <div>
+        <h1>Welcome to Inertia!</h1>
+        <p>You are using Laravel v{{ laravelVersion }} and PHP v{{ phpVersion }}</p>
+    </div>
+</template>
+
+<style scoped>
+/* Your component styles here */
+</style>
