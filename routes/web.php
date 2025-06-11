660f18913d9c904a9c4499d25522d1ebf024b0b6
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-06-11 09:37:19 +0900

    comment: disable routes for home, remote-login, and archives

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,21 @@ use Inertia\Inertia;
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::get('/', function () {
-    return Inertia::render('Home', [ // <-- 'Home' matches Home.vue
-        'laravelVersion' => 12,
-        'phpVersion' => 8.3,
-    ]);
-});
+//Route::get('/', function () {
+//    return Inertia::render('Home', [ // <-- 'Home' matches Home.vue
+//        'laravelVersion' => 12,
+//        'phpVersion' => 8.3,
+//    ]);
+//});
 
-Route::get('/remote-login', function () {
-    return view('remote-login');
-});
+//Route::get('/remote-login', function () {
+//    return view('remote-login');
+//});
 
 //Route::get('/login', [RemoteLoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
 //Route::post('/login', [RemoteLoginController::class, 'login'])->middleware('guest');
 //Route::post('/logout', [RemoteLoginController::class, 'logout'])->name('logout')->middleware('auth');
 
 //guest
-Route::get('/archives', [ArchiveController::class, 'index'])->name('archives.index');
-Route::post('/archives', [ArchiveController::class, 'index'])->name('archives.index');
+//Route::get('/archives', [ArchiveController::class, 'index'])->name('archives.index');
+//Route::post('/archives', [ArchiveController::class, 'index'])->name('archives.index');
