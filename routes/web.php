899d7a283081fc138b42886ec75f4e8cb3d52a82
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-06-11 09:30:04 +0900

    update: configure Vite with path aliases and improve Vue template asset handling

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RemoteLoginController;
+use App\Http\Controllers\Guest\ArchiveController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 //
@@ -18,7 +19,10 @@ Route::get('/remote-login', function () {
     return view('remote-login');
 });
 
-Route::get('/login', [RemoteLoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
-Route::post('/login', [RemoteLoginController::class, 'login'])->middleware('guest');
-Route::post('/logout', [RemoteLoginController::class, 'logout'])->name('logout')->middleware('auth');
+//Route::get('/login', [RemoteLoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
+//Route::post('/login', [RemoteLoginController::class, 'login'])->middleware('guest');
+//Route::post('/logout', [RemoteLoginController::class, 'logout'])->name('logout')->middleware('auth');
 
+//guest
+Route::get('/archives', [ArchiveController::class, 'index'])->name('archives.index');
+Route::post('/archives', [ArchiveController::class, 'index'])->name('archives.index');
