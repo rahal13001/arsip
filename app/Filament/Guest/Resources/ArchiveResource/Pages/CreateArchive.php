3a67c7f4cc9aed1a3a648c73d56d15ae56da5380
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-06-11 09:30:58 +0900

    update: add GuestPanelProvider and pages for creating and editing guest archives

---
+++ app/Filament/Guest/Resources/ArchiveResource/Pages/CreateArchive.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Guest\Resources\ArchiveResource\Pages;
+
+use App\Filament\Guest\Resources\ArchiveResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateArchive extends CreateRecord
+{
+    protected static string $resource = ArchiveResource::class;
+}
