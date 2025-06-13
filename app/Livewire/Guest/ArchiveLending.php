5e447f23f2fb52ed2bdabe04a26363a445d415c6
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-06-14 08:54:52 +0900

    update: adjust padding on Archive Lending page for consistency

--- app/Livewire/Guest/ArchiveLending.php
+++ app/Livewire/Guest/ArchiveLending.php
@@ -2,12 +2,14 @@
 
 namespace App\Livewire\Guest;
 
+use App\Models\Archive;
 use Livewire\Component;
 
 class ArchiveLending extends Component
 {
     // Properti publik untuk menampung data dari form
     public $applicant_name;
+    public $archive_id;
     public $applicant_position;
     public $applicant_organization;
     public $applicant_address;
@@ -48,12 +50,32 @@ class ArchiveLending extends Component
     /**
      * Fungsi yang dipanggil saat form di-submit.
      */
+    public function mount($record)
+    {
+        $this->archive_id = Archive::select('id', 'archive_slug')->where('archive_slug', $record)->first();
+
+
+    }
     public function save()
     {
         // Jalankan validasi
         $validatedData = $this->validate();
 
         try {
+            \App\Models\Archivelending::create([
+                'archive_id' => $this->archive_id->id,
+                'applicant_name' => $this->applicant_name,
+                'applicant_position' => $this->applicant_position,
+                'applicant_organization' => $this->applicant_organization,
+                'applicant_address' => $this->applicant_address,
+                'applicant_phone' => $this->applicant_phone,
+                'applicant_email' => $this->applicant_email,
+                'applicant_id_number' => $this->applicant_id_number,
+                'date_of_application' => $model->created_at ?? now(),
+                'date_of_lending' => $this->date_of_lending,
+                'lending_until' => $this->lending_until,
+                'applicant_note' => $this->applicant_note,
+            ]);
             // Logika untuk menyimpan data ke database.
             // Pastikan Anda sudah membuat model `ArchiveLoan` dan migrasinya.
             // Contoh:
@@ -63,7 +85,9 @@ class ArchiveLending extends Component
             session()->flash('success', 'Permohonan peminjaman arsip Anda telah berhasil dikirim. Kami akan segera menghubungi Anda.');
 
             // Kosongkan kembali form setelah berhasil
-            $this->reset();
+            return $this->redirect(route('lending_confirmation', ['record' => $this->archive_id->archive_slug]));
+//            $this->reset();
+
 
         } catch (\Exception $e) {
             // Tampilkan pesan error jika terjadi masalah saat menyimpan
