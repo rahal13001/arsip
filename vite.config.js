b1b9d1d3f9125871c836f2ac5fe20443c793f175
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-06-13 11:51:33 +0900

    remove: delete obsolete app-BOk-i4f-.css file from public/build/assets directory

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
