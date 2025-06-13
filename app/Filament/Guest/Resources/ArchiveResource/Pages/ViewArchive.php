8f73294495e153885726b59e240fe8c96d07feb0
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-06-14 08:55:27 +0900

    update: adjust Archive actions to reflect lending status dynamically

--- app/Filament/Guest/Resources/ArchiveResource/Pages/ViewArchive.php
+++ app/Filament/Guest/Resources/ArchiveResource/Pages/ViewArchive.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Guest\Resources\ArchiveResource\Pages;
 
 use App\Filament\Guest\Resources\ArchiveResource;
+use App\Models\Archivelending;
 use Filament\Actions;
 use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
@@ -13,11 +14,24 @@ class ViewArchive extends ViewRecord
 
     protected function getHeaderActions(): array
     {
+        $check = Archivelending::where('archive_id', $this->record->id)
+                    ->where('lending_approval', 1)
+                    ->where('return_date', null)
+                    ->count();
+
         return [
             Action::make('lending')
+                ->hidden($check > 0)
                 ->label('Pinjam Arsip')
                 ->icon('heroicon-o-document-arrow-down')
-                ->url(fn (): string => $this->getResource()::getUrl('index') . '/' . $this->record->getRouteKey() . '/lending'),
+                ->url(fn (): string => $this->getResource()::getUrl('index') . '/' . $this->record->getRouteKey() . '/pinjam-arsip'),
+
+            Action::make('return')
+                ->hidden($check == 0)
+                ->color('danger')
+                ->label('Arsip Sedang Dipinjam')
+                ->icon('heroicon-o-document-arrow-up')
+                ->disabled()
 
         ];
     }
